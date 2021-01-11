ad796f210051c6c50548f03ed4d386f164ab3100
manuzcheruz <kipkemoi.emanuel@gmail.com>
2021-01-11 14:06:41 +0300

    created apps

--- shake/settings.py
+++ shake/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,8 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '**^x*vp=w3m&(ealf%jotatxne%9c588!^y0!kcc+_pn81)^%_'
-
+# SECRET_KEY = '**^x*vp=w3m&(ealf%jotatxne%9c588!^y0!kcc+_pn81)^%_'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -37,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'centers',
+    'employers',
+    'students',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +80,10 @@ WSGI_APPLICATION = 'shake.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'handshake',
+        'USER': 'manuz',
+        'PASSWORD': 'disneyboy',
     }
 }
 
